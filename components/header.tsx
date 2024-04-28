4f2343003cb43807f751035df066ecaa9b7fb481
Tomás Delgado <tmsdlgd@gmail.com>
2024-04-28 18:18:38 -0400

    cursor

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ import Router from "next/router";
 const Header = () => {
     return (
         <header className="bg-gray-800 text-white py-4">
-            <div className="container mx-auto cursor-pointer" onClick={() => Router.push("/", `/`)}>
-                <h1 className="text-2xl font-semibold">Demos</h1>
+            <div className="container mx-auto">
+                <h1 className="text-2xl font-semibold cursor-pointer inline-block cursor-pointer" onClick={() => Router.push("/", `/`)}>Demos</h1>
             </div>
         </header>
     );
