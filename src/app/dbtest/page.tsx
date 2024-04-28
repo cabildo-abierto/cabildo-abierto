1420b91cda967b955ff997f4ead401ca649b61be
Tomás Delgado <tmsdlgd@gmail.com>
2024-04-28 12:44:17 -0400

    prisma + vercel en progreso

--- src/app/dbtest/page.tsx
+++
@@ -1,8 +0,0 @@
-import prisma from '../lib/prisma'
-
-export const getServerSideProps = async () => {
-    const users = await prisma.User.findMany()
-    console.log(users)
-
-    return { props: { users } }
-}
--- src/app/header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <header className="bg-gray-800 py-4">
-            <div className="container mx-auto px-4">
-                <h1 className="text-white text-2xl font-semibold">Demos</h1>
-            </div>
-        </header>
-    );
-};
