448e27d9a6daa433bd7d0a01353f38916e1895a2
Tomás Delgado <tdelgado@gmail.com>
2024-07-02 15:07:01 -0300

    editor wiki

--- src/app/entidad/[id]/entity_page.tsx
+++ src/app/entidad/[id]/entity_page.tsx
@@ -1,5 +1,8 @@
 "use client"
 import { updateEntityContent } from "@/actions/create-entity"
+import MarkdownEditor from "@/components/editor/markdown-editor";
+import ReadOnlyEditor from "@/components/editor/read-only-editor";
+import ReadOnlyMarkdownEditor from "@/components/editor/read-only-markdown-editor";
 import MarkdownContent from "@/components/markdown-content"
 import { useState } from "react"
 import TextareaAutosize from 'react-textarea-autosize';
@@ -39,14 +42,18 @@ const EntityPage = ({entity}) => {
             
         </div>
         <div className="px-2 mb-2">
-        {modify ? <TextareaAutosize 
-            className="w-full border-0 focus:outline-none px-1 py-1 resize-none"
-            value={content}
-            onChange={(e) => {setContent(e.target.value)}}
-        /> : 
-        <MarkdownContent content={content}/>}
+            {modify ? <MarkdownEditor initialData={content} onChange={(x) => {setContent(x);}}/> : 
+            <ReadOnlyMarkdownEditor content={content}/>}
         </div>
     </>
 }
+
+
+/*{modify ? <TextareaAutosize 
+    className="w-full border-0 focus:outline-none px-1 py-1 resize-none"
+    value={content}
+    onChange={(e) => {setContent(e.target.value)}}
+/> : 
+<MarkdownContent content={content}/>}*/
 
 export default EntityPage
--- src/components/markdown-content.tsx
+++ src/components/markdown-content.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import 'ckeditor5/ckeditor5.css';
 
 export default function ({content}) {
     return <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
