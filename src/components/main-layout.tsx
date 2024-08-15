cef2509f37c01084e464ecacfe64ee3925969f6c
Tomás Delgado <tdelgado@gmail.com>
2024-08-15 19:43:16 -0300

    boton ver comentarios celu

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -8,19 +8,17 @@ type ColumnsProps = {left?: ReactNode, center?: ReactNode, right?: ReactNode, ce
 export const ThreeColumnsLayout: React.FC<ColumnsProps> = (
     {left=null, center=null, right=null, centerWidth=800}) => {
     
-    return (
-        <div className="flex justify-center">
-            <div className="hidden lg:block lg:flex-1">
-                {left}
-            </div>
-            <div className="w-full lg:w-[800px]">
-                {center}
-            </div>
-            <div className="hidden lg:block lg:flex-1">
-                {right}
-            </div>
+    return <div className="flex justify-center">
+        <div className="hidden lg:block lg:flex-1">
+            {left}
         </div>
-    );
+        <div className="w-full lg:w-[800px]">
+            {center}
+        </div>
+        <div className="hidden lg:block lg:flex-1">
+            {right}
+        </div>
+    </div>
 }
 
 
