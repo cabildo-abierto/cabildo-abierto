494846170fbee1d4c0b0341fe0ec154b8252b7ed
Tomás Delgado <tdelgado@gmail.com>
2024-08-15 19:54:12 -0300

    formato fecha

--- src/components/date.tsx
+++ src/components/date.tsx
@@ -30,11 +30,42 @@ export function DateSince({ date }: { date: Date }) {
 
     const now = new Date();
     const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-    const minutes = Math.floor(seconds / 60);
-    console.log(seconds, minutes)
-    const localeDate = seconds < 10 ? 'ahora' : rtf.format(-minutes, 'minute');
     
-    return <>{localeDate}</>;
+    const formatTime = () => {
+        if (seconds < 60) {
+            return rtf.format(-seconds, 'second');
+        }
+        
+        const minutes = Math.floor(seconds / 60);
+        if (minutes < 60) {
+            return rtf.format(-minutes, 'minute');
+        }
+        
+        const hours = Math.floor(minutes / 60);
+        if (hours < 24) {
+            return rtf.format(-hours, 'hour');
+        }
+        
+        const days = Math.floor(hours / 24);
+        if (days < 7) {
+            return rtf.format(-days, 'day');
+        }
+        
+        const weeks = Math.floor(days / 7);
+        if (weeks < 4) {
+            return rtf.format(-weeks, 'week');
+        }
+        
+        const months = Math.floor(days / 30);
+        if (months < 12) {
+            return rtf.format(-months, 'month');
+        }
+        
+        const years = Math.floor(days / 365);
+        return rtf.format(-years, 'year');
+    };
+
+    return <>{formatTime()}</>;
 }
 
 // Componente para mostrar la fecha y la hora
