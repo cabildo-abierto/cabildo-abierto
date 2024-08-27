35260f7b9e063ffda02d04c16fd90b485de9284d
Tomás Delgado <tdelgado@gmail.com>
2024-08-27 01:21:19 -0300

    presentacion hover, usernames

--- src/app/perfil/[id]/page.tsx
+++ src/app/perfil/[id]/page.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import { ThreeColumnsLayout } from "@/components/three-columns";
 import { ProfileHeader } from "@/components/profile-header";
 import { ErrorPage } from "@/components/error-page";
-import { getUserById } from "@/actions/get-user";
+import { getUser, getUserById } from "@/actions/get-user";
 import { ProfileFeed } from "./profile-feed";
 
 
 const UserProfile: React.FC<{ params: { id: string } }> = async ({ params }) => {
-    const username = "@"+decodeURIComponent(params?.id)
-    const user = await getUserById(username)
+    const username = decodeURIComponent(params?.id)
+    
+    const user = await getUserById(username == "tdelgado" ? "@tdelgado" : username)
     if (!user) {
-        return <ErrorPage>El usuario {username} no existe</ErrorPage>
+        return <ErrorPage>El usuario @{username} no existe</ErrorPage>
     }
+
+    const loggedInUser = await getUser()
 
     const center = <>
         {user && <ProfileHeader
-            profileUser={user}
+            profileUser={user} user={loggedInUser}
         />}
         <ProfileFeed profileUser={user}/>
     </>
--- src/app/presentation.tsx
+++ src/app/presentation.tsx
@@ -4,6 +4,7 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { SignupButton } from '@/components/home-page';
 import { ReactNode } from 'react';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import Explainable from '@/components/explainable';
 
 export const PeriodoDePrueba = () => {
     return <div className="flex justify-center">
@@ -36,14 +37,6 @@ const Explanation = ({children, id, title}: {children: ReactNode, id: string, ti
         {children}
         </div>
     </div>
-}
-
-const Explainable = ({text, id}: {text: string, id?: string}) => {
-    return <Link
-        href={"#" + (id ? id : text)}
-        className="hover:text-[var(--primary)] cursor-pointer">
-        {text}
-    </Link>
 }
 
 
@@ -90,11 +83,50 @@ const Table = () => {
 
 
 export const Presentation: React.FC = () => {
+    const informacion = <div className="">
+        <p>El contenido se compone de:</p>
+        <p className="py-2"><span className="">Artículos informativos escritos colaborativamente</span>, que representan consensos.</p>
+        <p className="py-2"><span className="">Publicaciones individuales</span>, que pueden contener apreciaciones personales, análisis, relatos, o lo que sea.</p>
+    </div>
+
+    const abierta = <div className="flex flex-col items-center text-center">
+    <p>Todo está abierto a discusión.</p>
+    <p> En cualquier artículo o publicación vas a poder agregar comentarios, incluso sobre selecciones de texto.</p>
+    <p> El diseño de la plataforma también está abierto a discusión.</p>
+</div>
+
+    const escrita = <div className="flex justify-center text-center">
+    <p>
+        Además de agregar comentarios, cualquier usuario puede contribuir con contenido y recibir una remuneración en función del valor que otros usuarios encuentran en lo que escribe.
+    </p>
+</div>
+
+    const financiada = <div className="flex flex-col justify-center text-center">
+        <p>Cabildo Abierto se financia exclusivamente con suscripciones mensuales de sus usuarios.</p>
+        <p>Con esto se cubre el desarrollo de la plataforma y el trabajo de los autores.</p>
+        <p>Hay además un pozo de suscripciones gratuitas del que el que lo necesite puede agarrar.</p>
+    </div>
+
+    const personas = <div className="flex flex-col justify-center text-center">
+    <p>En Cabildo Abierto los usuarios son personas se hacen cargo de lo que dicen, como en la vida real.</p>
+    <p>No hay bots en la discusión y tampoco segundas cuentas.</p>
+    </div>
+
+    const algoritmos = <div className="flex flex-col items-center text-center">
+    <p>No usamos inteligencia artificial para elegir el contenido que ven los usuarios en función de su comportamiento previo.</p>
+    <p>La selección y priorización de contenido está exclusivamente bajo tu control.</p>
+</div>
+
+    const noticias = <div className="flex flex-col items-center text-center">
+    <p>La libertad de expresión es uno de los motivadores centrales de esta plataforma, pero no es un derecho absoluto.</p>
+    <p>Si ves una publicación falsa, podés marcarlo para que otros lo vean, abriendo una discusión al respecto.</p>
+</div>
+
     return <div className="flex flex-col items-center text-gray-900">
         <div className="flex justify-center">
             <div className="flex flex-col justify-center">
                 <h3 className="flex justify-center text-center mt-20 lg:text-4xl md:text-4xl text-3xl px-2">
-                    Una plataforma para la discusión pública
+                    Un nuevo medio para la discusión pública
                 </h3>
             </div>
         </div>
@@ -102,68 +134,19 @@ export const Presentation: React.FC = () => {
         <div className="flex flex-col items-center py-16  max-w-[800px]">
             <div className="bg-gray-200 rounded px-8 py-3">
             <div className="mb-2 text-center text-xl">
-                <Explainable text="Información" id="informacion"/> <Explainable text="abierta a discusión" id="abierta"/>. 
+                <Explainable text="Información" content={informacion}/> <Explainable text="abierta a discusión" content={abierta}/>. 
             </div>
             <div className="text-center text-xl mb-2">
-                <Explainable text="Escrita"/> y <Explainable text="financiada"/> por la comunidad.
+                <Explainable text="Escrita" content={escrita}/> y <Explainable text="financiada" content={financiada}/> por la comunidad.
             </div>
             </div>
             <div className="text-center text-lg mt-8 text-gray-800">
-                Con <Explainable text="personas reales"/> y <Explainable text="sin algoritmos"/> <Explainable text="ni noticias falsas"/>.
+                Con <Explainable text="personas reales" content={personas}/> y <Explainable text="sin algoritmos" content={algoritmos}/> <Explainable text="ni noticias falsas" content={noticias}/>.
             </div>
         </div>
 
         <div className="flex justify-center">
-            <SignupButton className="w-64 h-12 gray-btn font-bold mb-16" text="Empezar"/>
-        </div>
-
-        <div className="flex flex-col items-center max-w-[800px]">
-        <Explanation id="informacion" title="Información">
-            <div className="flex justify-center text-center">
-                <p>Vas a encontrar artículos informativos escritos colaborativamente, que representan consensos y publicaciones de usuarios individuales, que pueden contener apreciaciones personales, análisis, relatos, etc.
-                </p>
-                
-            </div>
-        </Explanation>
-        <Explanation id="abierta" title="Abierta a discusión">
-            <div className="flex flex-col items-center text-center">
-                <p>Todo está abierto a discusión.</p><p> En cualquier artículo o publicación vas a poder agregar comentarios, incluso sobre selecciones de texto.</p>
-            </div>
-        </Explanation>
-        <Explanation id="escrita" title="Escrita por la comunidad">
-            <div className="flex justify-center text-center">
-                <p>
-                    Además de agregar comentarios, cualquier usuario puede contribuir con contenido y recibir una remuneración en función del valor que otros usuarios encuentran en lo que escribe.
-                </p>
-            </div>
-        </Explanation>
-        <Explanation id="financiada" title="Financiada por la comunidad">
-            <div className="flex flex-col justify-center text-center">
-                <p>Cabildo Abierto se financia exclusivamente con suscripciones mensuales de sus usuarios.</p>
-                <p>Con esto se cubre el desarrollo de la plataforma y el trabajo de los autores.</p>
-                <p>Hay además un pozo de suscripciones gratuitas del que el que lo necesite puede agarrar.</p>
-            </div>
-        </Explanation>
-        <Explanation id="personas reales" title="Con personas reales">
-            <div className="flex flex-col justify-center text-center">
-            <p>En Cabildo Abierto los usuarios son personas se hacen cargo de lo que dicen, como en la vida real.</p>
-            <p>No hay bots en la discusión y tampoco segundas cuentas.</p>
-            </div>
-        </Explanation>
-        <Explanation id="sin algoritmos" title="Sin algoritmos">
-            <div className="flex flex-col items-center text-center">
-                <p>No usamos inteligencia artificial para elegir el contenido que consumen los usuarios en función de su comportamiento previo.</p>
-                <p>La selección y priorización de contenido está exclusivamente bajo tu control.</p>
-            </div>
-        </Explanation>
-        <Explanation id="ni noticias falsas" title="Sin noticias falsas">
-            <div className="flex flex-col items-center text-center">
-                <p>La libertad de expresión es uno de los motivadores centrales de esta plataforma, pero no es un derecho absoluto.</p>
-
-                <p>En Cabildo Abierto podés denunciar publicaciones falsas dando los argumentos correspondientes, abriendo una discusión transparente sobre su veracidad.</p>
-            </div>
-                
-        </Explanation>
+            <SignupButton className="w-64 h-12 gray-btn font-bold" text="Empezar"/>
         </div>
     </div>
 };
