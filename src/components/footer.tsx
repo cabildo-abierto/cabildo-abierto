c946fe12073313d2efb3bdb5b6dea7a35d7f0a58
Tomás Delgado <tdelgado@gmail.com>
2024-09-01 12:12:11 -0300

    fix cache

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,9 @@
 export default function Footer() {
     return (
         <div className="fixed bottom-0">
-        <footer className="px-2 border-t h-6 w-screen">
+        <footer className="px-2 h-6 w-screen text-gray-800">
             <div className="flex justify-end items-center">
-                <span className="mr-1">Escribinos a </span><a href="mailto:contacto@cabildoabierto.com.ar">contacto@cabildoabierto.com.ar</a>
+                <a href="mailto:contacto@cabildoabierto.com.ar">contacto@cabildoabierto.com.ar</a>
                 {false && <div className="flex space-x-4">
                     <a href="#" className="hover:text-blue-500">Política de privacidad</a>
                     <a href="#" className="hover:text-blue-500">Términos de servicio</a>
