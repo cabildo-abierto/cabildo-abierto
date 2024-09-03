fb0f08d54b352195c531f04f6509532c766d998c
Tomás Delgado <tdelgado@gmail.com>
2024-09-03 00:38:05 -0300

    lcs x2

---
+++ src/components/editor/nodes/DiffNode.tsx
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import type {
+    BaseSelection,
+    EditorConfig,
+    LexicalNode,
+    NodeKey,
+    RangeSelection,
+    SerializedElementNode,
+    Spread,
+  } from 'lexical';
+  
+  import {
+    addClassNamesToElement,
+    removeClassNamesFromElement,
+  } from '@lexical/utils';
+  import {$applyNodeReplacement, $isRangeSelection, ElementNode} from 'lexical';
+  
+  export type SerializedDiffNode = Spread<
+    {
+      kind: string;
+    },
+    SerializedElementNode
+  >;
+  
+  /** @noInheritDoc */
+  export class DiffNode extends ElementNode {
+    /** @internal */
+    __kind: string;
+  
+    static getType(): string {
+      return 'diff';
+    }
+  
+    static clone(node: DiffNode): DiffNode {
+      return new DiffNode(node.__kind, node.__key);
+    }
+  
+    static importDOM(): null {
+      return null;
+    }
+  
+    static importJSON(serializedNode: SerializedDiffNode): DiffNode {
+      const node = $createDiffNode(serializedNode.kind);
+      node.setFormat(serializedNode.format);
+      node.setIndent(serializedNode.indent);
+      node.setDirection(serializedNode.direction);
+      return node;
+    }
+  
+    exportJSON(): SerializedDiffNode {
+      return {
+        ...super.exportJSON(),
+        kind: this.getKind(),
+        type: 'diff',
+        version: 1,
+      };
+    }
+  
+    constructor(kind: string, key?: NodeKey) {
+      super(key);
+      this.__kind = kind || "";
+    }
+  
+    createDOM(config: EditorConfig): HTMLElement {
+      const element = document.createElement('div');
+      if(this.__kind == "new"){
+        addClassNamesToElement(element, "diffNew")
+      } else if(this.__kind == "removed"){
+        addClassNamesToElement(element, "diffRemoved")
+      } else if(this.__kind == "modified"){
+        addClassNamesToElement(element, "diffModified")
+      }
+      return element;
+    }
+  
+    updateDOM(
+      prevNode: DiffNode,
+      element: HTMLElement,
+      config: EditorConfig,
+    ): boolean {
+      const prevKind = prevNode.__kind;
+      const nextKind = this.__kind;
+        
+      return false;
+    }
+
+    getKind(): string {
+      const self = this.getLatest();
+      return $isDiffNode(self) ? self.__kind : "";
+    }
+  
+    insertNewAfter(
+      selection: RangeSelection,
+      restoreSelection = true,
+    ): null | ElementNode {
+      const DiffNode = $createDiffNode(this.__kind);
+      this.insertAfter(DiffNode, restoreSelection);
+      return DiffNode;
+    }
+  
+    canInsertTextBefore(): false {
+      return false;
+    }
+  
+    canInsertTextAfter(): false {
+      return false;
+    }
+  
+    canBeEmpty(): false {
+      return false;
+    }
+  
+    isInline(): true {
+      return true;
+    }
+  
+    extractWithChild(
+      child: LexicalNode,
+      selection: BaseSelection,
+      destination: 'clone' | 'html',
+    ): boolean {
+      if (!$isRangeSelection(selection) || destination === 'html') {
+        return false;
+      }
+      const anchor = selection.anchor;
+      const focus = selection.focus;
+      const anchorNode = anchor.getNode();
+      const focusNode = focus.getNode();
+      const isBackward = selection.isBackward();
+      const selectionLength = isBackward
+        ? anchor.offset - focus.offset
+        : focus.offset - anchor.offset;
+      return (
+        this.isParentOf(anchorNode) &&
+        this.isParentOf(focusNode) &&
+        this.getTextContent().length === selectionLength
+      );
+    }
+  
+    excludeFromCopy(destination: 'clone' | 'html'): boolean {
+      return destination !== 'clone';
+    }
+  }
+  
+  export function $createDiffNode(kind: string): DiffNode {
+    return $applyNodeReplacement(new DiffNode(kind));
+  }
+  
+  export function $isDiffNode(node: LexicalNode | null): node is DiffNode {
+    return node instanceof DiffNode;
+  }
+
+
---
+++ src/components/min-cost-flow.ts
@@ -0,0 +1,85 @@
+import { makeMatrix } from "./diff";
+
+const INF = 1e9
+export function assignment(a: Array<Array<number>>): Array<number> {
+    let n = a.length
+    let m = n * 2 + 2;
+    let f = makeMatrix(m, m, 0)
+    let s = m - 2
+    let t = m - 1
+    let cost = 0
+
+    while (true) {
+        let dist = new Array<number>(m).fill(INF)
+
+        let p = new Array<number>(m);
+        let inq = new Array<boolean>(m).fill(false)
+        let q = []
+        dist[s] = 0
+        p[s] = -1
+        q.push(s)
+        while (q.length > 0) {
+            let v = q[0];
+            q = q.slice(1);
+            inq[v] = false;
+            if (v == s) {
+                for (let i = 0; i < n; ++i) {
+                    if (f[s][i] == 0) {
+                        dist[i] = 0;
+                        p[i] = s;
+                        inq[i] = true;
+                        q.push(i);
+                    }
+                }
+            } else {
+                if (v < n) {
+                    for (let j = n; j < n + n; ++j) {
+                        if (f[v][j] < 1 && dist[j] > dist[v] + a[v][j - n]) {
+                            dist[j] = dist[v] + a[v][j - n];
+                            p[j] = v;
+                            if (!inq[j]) {
+                                q.push(j);
+                                inq[j] = true;
+                            }
+                        }
+                    }
+                } else {
+                    for (let j = 0; j < n; ++j) {
+                        if (f[v][j] < 0 && dist[j] > dist[v] - a[j][v - n]) {
+                            dist[j] = dist[v] - a[j][v - n];
+                            p[j] = v;
+                            if (!inq[j]) {
+                                q.push(j);
+                                inq[j] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        let curcost = INF;
+        for (let i = n; i < n + n; ++i) {
+            if (f[i][t] == 0 && dist[i] < curcost) {
+                curcost = dist[i];
+                p[t] = i;
+            }
+        }
+        if (curcost == INF)
+            break;
+        cost += curcost;
+        for (let cur = t; cur != -1; cur = p[cur]) {
+            let prev = p[cur];
+            if (prev != -1)
+                f[cur][prev] = -(f[prev][cur] = 1);
+        }
+    }
+    let answer = new Array(n);
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n; ++j) {
+            if (f[i][j + n] == 1)
+                answer[i] = j;
+        }
+    }
+    return answer;
+}
