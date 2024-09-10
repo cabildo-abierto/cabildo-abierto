256a336943bee00c95b5d658810bba7dacfb86eb
Tomás Delgado <tdelgado@gmail.com>
2024-09-10 18:08:12 -0300

    async inicio

--- src/app/inicio/[[...route]]/page.tsx
+++ src/app/inicio/[[...route]]/page.tsx
@@ -1,27 +1,29 @@
-"use client"
-import React, { useState } from "react"
+import React from "react"
 import { ThreeColumnsLayout } from "src/components/three-columns";
-import { useRouter } from "next/navigation";
-import { useUsers } from "src/app/hooks/user";
-import { useRouteFeed, useRouteFollowingFeed, useRouteEntities } from "src/app/hooks/contents";
 import { RouteContent } from "src/components/route-content";
+import { getRouteEntities, getRouteFeed, getRouteFollowingFeed, getUserId, getUsers } from "src/actions/actions";
 
 
 
 const TopicsPage: React.FC<{
     params: {route: string[]}
     searchParams: { [key: string]: string }
-}> = ({params, searchParams}) => {
+}> = async ({params, searchParams}) => {
     const decodedRoute = params.route ? params.route.map(decodeURIComponent) : []
-    const [selected, setSelected] = useState(searchParams.selected ? searchParams.selected : "General")
 
-    const preloadEntities = useRouteEntities(decodedRoute)
-    const preloadUsers = useUsers()
-    const preloadFeed = useRouteFeed(decodedRoute)
-    const preloadFollowingFeed = useRouteFollowingFeed(decodedRoute)
-    const router = useRouter()
+    const userId = await getUserId()
+    const feed = await getRouteFeed(decodedRoute)
+    const followingFeed = await getRouteFollowingFeed(decodedRoute, userId)
+    const routeEntities = await getRouteEntities(decodedRoute)
+    const users = await getUsers()
 
-    const center = <RouteContent route={decodedRoute}/>
+    const center = <RouteContent
+        route={decodedRoute}
+        feed={feed}
+        followingFeed={followingFeed}
+        routeEntities={routeEntities}
+        users={users}
+    />
 
     return <ThreeColumnsLayout center={center} />
 }
