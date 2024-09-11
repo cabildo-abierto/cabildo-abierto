e4e5cf201417d580b28d02f37ef2f3952eff1465
Tomás Delgado <tdelgado@gmail.com>
2024-09-11 00:16:32 -0300

    blur paywall

--- src/components/paywall-checker.tsx
+++ src/components/paywall-checker.tsx
@@ -13,7 +13,7 @@ const NeedAccountPaywall: React.FC<any> = ({ children }) => {
             <div className="relative z-0">
                 {children}
             </div>
-            <div className="fixed inset-0 bg-opacity-50 bg-gray-800 z-10 flex justify-center items-center">
+            <div className="fixed inset-0 bg-opacity-50 bg-gray-800 z-10 flex justify-center items-center backdrop-blur-sm">
                 
                 <div className="bg-[var(--background)] rounded border-2 border-black p-8 z-10 text-center max-w-lg">
                     <div className="py-4 text-lg">Iniciá sesión o creá una cuenta para ver esta página</div>
@@ -38,8 +38,7 @@ const NeedSubscriptionPaywall: React.FC<any> = ({ children }) => {
             <div className="relative z-0">
                 {children}
             </div>
-            <div className="fixed inset-0 bg-opacity-50 bg-gray-800 z-10 flex justify-center items-center">
-                
+            <div className="fixed inset-0 bg-opacity-50 bg-gray-800 z-10 flex justify-center items-center backdrop-blur-sm">
                 <div className="bg-[var(--background)] rounded border-2 border-black p-8 z-10 text-center max-w-lg">
                     <div className="py-4 text-lg">Necesitás una suscripción activa para ver esta página</div>
                     <div className="flex justify-center items-center py-8 space-x-4">
