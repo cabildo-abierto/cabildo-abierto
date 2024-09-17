a886da05c898fca9b9023b281df25e683dde26b0
Tomás Delgado <tmsdlgd@gmail.com>
2024-09-17 19:21:52 -0300

    leyendo el json en post

--- src/app/api/pago/route.ts
+++ src/app/api/pago/route.ts
@@ -2,13 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req) {
     console.log("got a payment!");
-    console.log("request is", req)
-    if (req.method === 'POST') {
-        console.log("it was a post")
-        console.log("body", req.body)
-        console.log("parsed", JSON.parse(req.body))
-        const { type, data } = JSON.parse(req.body);
-    }
+    const json = await req.json()
+
+    console.log("json", json)
 
     return NextResponse.json({ status: 200 });
 }
