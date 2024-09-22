b5daacdb1baf90dfd395177ddba13dcc396eaed4
Tomás Delgado <tmsdlgd@gmail.com>
2024-09-22 12:49:21 -0300

    logo

--- src/app/soporte/page.tsx
+++ src/app/soporte/page.tsx
@@ -10,6 +10,8 @@ import { ThreeColumnsLayout } from "../../components/three-columns";
 export default async function Page() {
     const user = await getUser()
 
+    if(!user) return <></>
+    
     const center = <div className="text-center">
         <h1 className="text-xl pt-6 pb-2">Chateá con el equipo de Cabildo Abierto</h1>
         <div className="text-gray-600 text-base">
