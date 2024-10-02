94c86469690f071a0cf063b4d8c77c8a98f8175b
Tomás Delgado <tmsdlgd@gmail.com>
2024-10-02 11:44:08 -0300

    limite 1.2m, fix ancestor content

--- src/components/save-edit-popup.tsx
+++ src/components/save-edit-popup.tsx
@@ -45,7 +45,7 @@ export const SaveEditPopup = ({
 
     const newVersionSize = getAllText(JSON.parse(jsonState).root).length
 
-    if(newVersionSize > 500000){
+    if(newVersionSize > 1200000){
         return <AcceptButtonPanel
             text={<span>No se pueden guardar los cambios porque el artículo supera el límite de 500.000 caracteres (con <span className="text-red-600">{newVersionSize}</span> caracteres). Te sugerimos que separes el contenido en secciones en distintos artículos.</span>}
             onClose={onClose}
