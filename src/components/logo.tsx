580cf313d0821c1aa1e785ca0c2fb00a7edc10c9
Tomás Delgado <tmsdlgd@gmail.com>
2024-10-02 14:08:39 -0300

    logo nuevo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 export const LogoWithName = ({showName}: {showName: boolean}) => {
     return <div><Link href="/"><div className="flex items-center ml-1">
         <Image
-          src="/cabildo-icono.svg"
+          src="/logo.svg"
           alt="Loading..."
           width={314}
           height={314}
           priority={true}
-          className="w-10 h-10 rounded-sm"
+          className="w-10 h-10 rounded"
         />
         {showName && <div className="ml-1 items-end text-lg text-gray-900 hidden sm:flex">
             <div>Cabildo Abierto</div>
@@ -21,12 +21,34 @@ export const LogoWithName = ({showName}: {showName: boolean}) => {
 
 export const Logo = ({className, opacity=1}: {className: string, opacity?: number}) => {
     return <Image
-          src="/cabildo-icono.svg"
+          src="/logo.svg"
           alt="Loading..."
           width={397}
           height={397}
           priority={true}
-          className={"object-contain rounded-sm "+className}
+          className={"object-contain "+className}
           style={{opacity: opacity}}
     />
+}
+
+
+export function TopbarLogo() {
+    return <div className="hover:bg-[var(--secondary-light)] rounded-lg h-10 py-1 px-2 w-24 flex justify-center items-center">
+        <Link href="/">
+            <div className="flex items-center">
+                <Image
+                    src="/logo.svg"
+                    alt="Loading..."
+                    width={320}
+                    height={320}
+                    priority={true}
+                    className="w-8 h-8 rounded-sm"
+                />
+                <div className="ml-1 text-xs text-gray-900">
+                    <div>Cabildo</div>
+                    <div>Abierto</div>
+                </div>
+            </div>
+        </Link>
+    </div>
 }
