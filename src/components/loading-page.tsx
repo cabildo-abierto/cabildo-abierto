231ba948cf5511d5a22012012dbaf2b77f27ceaa
Tomás Delgado <tmsdlgd@gmail.com>
2024-10-05 21:53:58 -0300

    detalles

--- src/components/loading-page.tsx
+++ src/components/loading-page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import ReadOnlyEditor from './editor/read-only-editor';
 import { Logo } from './logo';
 import { useUser } from '../app/hooks/user';
+import { preload } from 'swr';
+import { fetcher } from '../app/hooks/utils';
 
 
 const LoadingPage: React.FC<{children: ReactNode}> = ({children}) => {
@@ -11,6 +13,15 @@ const LoadingPage: React.FC<{children: ReactNode}> = ({children}) => {
   if(user.isLoading){
     console.log("user is loading")
   }
+  useEffect(() => {
+      preload("/api/users", fetcher)
+      preload("/api/entities", fetcher)
+
+      // probablemente estos dos no tenga sentido ponerlos acá
+      preload("/api/feed/", fetcher)
+      preload("/api/following-feed/", fetcher)
+  }, [])
+
   return <>{(user.isLoading) ? 
     <div className="flex flex-col justify-center items-center w-screen h-screen">
       <div className="relative">
