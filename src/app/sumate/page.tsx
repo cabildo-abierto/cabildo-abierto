7d84b71bbb6100872233e0aa9ce211f43e8039ed
Tomás Delgado <tmsdlgd@gmail.com>
2024-10-13 19:10:55 -0300

    trabajá con nosotros

---
+++ src/app/sumate/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { ThreeColumnsLayout } from "../../components/three-columns";
+import { TopbarLogo } from "../../components/logo";
+
+
+
+export default function Page() {
+
+    const center = <div className="flex justify-center">
+        <div className="fixed top-0 left-0 w-screen">
+            <div className={"topbar-container"}>
+                <div className="px-2">
+                    <TopbarLogo/>
+                </div>
+            </div>
+        </div>
+
+        <div className="max-w-72 sm:max-w-[512px] flex flex-col items-center">
+            <h1 className="py-8 mt-12 text-3xl text-center">Trabajá con nosotros</h1>
+
+            <div className="py-4 text-lg text-center">
+                Nuestra misión es construir la mejor herramienta comunicacional posible para la discusión pública.
+            </div>
+
+            <div className="py-4 text-lg link text-center">
+                Si te interesa sumarte al equipo, escribinos a <Link href="mailto:contacto@cabildoabierto.com.ar">contacto@cabildoabierto.com.ar</Link>.
+            </div>
+        </div>
+    </div>
+
+    return <ThreeColumnsLayout center={center}/>
+}
