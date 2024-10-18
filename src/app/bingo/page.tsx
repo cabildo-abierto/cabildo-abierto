114c8f81c5d353ec9c1879169842f8c0931fd9ff
Tomás Delgado <tmsdlgd@gmail.com>
2024-10-18 18:56:38 -0300

    bingo

--- src/app/bingo/page.tsx
+++ src/app/bingo/page.tsx
@@ -24,8 +24,7 @@ const Page = () => {
     
     return (
         <div className="flex justify-between w-screen">
-
-            <div className="w-64 flex items-end h-screen px-2">
+            <div className="hidden w-64 lg:flex items-end h-screen px-2">
                 <div className="flex flex-col items-center">
                     <div className="mb-4 flex flex-col items-center">
                         <Image
@@ -39,9 +38,10 @@ const Page = () => {
                         </span>
                         <Link href="/inicio" className="flex items-center flex-col">
                             <Logo className="w-12 h-12" />
-                            <span className="text-sm text-gray-800 mb-2">
-                                www.cabildoabierto.com.ar
-                            </span>
+                            <div className="text-sm text-gray-800 mb-2 text-center">
+                                <p className="text-xs">en colaboración con</p>
+                                <p>www.cabildoabierto.com.ar</p>
+                            </div>
                         </Link>
                     </div>
                 </div>
@@ -49,19 +49,19 @@ const Page = () => {
             <div className="p-4 flex justify-center items-center flex-col">
                 <div className="flex justify-center items-center px-2">
                     <h2 
-                        className="text-gray-800 hover:text-blue-600 cursor-pointer text-4xl font-bold"
+                        className="text-gray-800 hover:text-blue-600 cursor-pointer text-4xl font-bold text-center"
                         onClick={handleBingoClick}
                     >
                         Bingo!
                     </h2>
                 </div>
 
-                <div className="grid grid-cols-10 gap-2 w-[600px] h-[600px] mt-4">
+                <div className="grid grid-cols-10 gap-2 lg:w-[600px] lg:h-[600px] mt-4">
                     {toggledButtons.map((isToggled, index) => (
                         <button
                             key={index}
                             onClick={() => toggleButton(index)}
-                            className={`w-12 h-12 border border-gray-500 text-lg rounded font-bold ${isToggled ? 'bg-green-400' : 'bg-white'}`}
+                            className={`lg:w-12 lg:h-12 border border-gray-500 lg:text-lg rounded ${isToggled ? 'bg-green-400' : 'bg-white'}`}
                         >
                             {index + 1}
                         </button>
@@ -77,7 +77,7 @@ const Page = () => {
                     </div>
                 )}
             </div>
-            <div className="w-64 flex flex-col items-center justify-end h-screen px-4">
+            <div className="hidden w-64 lg:flex flex-col items-center justify-end h-screen px-4">
                 <div className="text-xl text-gray-800 mb-4 flex flex-col items-center">
                     <h3 className="font-bold text-2xl">Cartones</h3>
                     <p>1 x $1000</p>
