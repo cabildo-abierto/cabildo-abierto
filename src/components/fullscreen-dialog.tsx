19f3e6d1dd2d86daa06468652171a1a679882c99
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-03 11:11:01 -0300

    write panel modal full screen

---
+++ src/components/fullscreen-dialog.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+import { Dialog } from "@headlessui/react";
+import { CloseButton } from "./close-button";
+
+export const FullscreenDialog = ({
+    children,
+    closeButton = false,
+    onClose,
+    className
+}: {
+    children: ReactNode;
+    closeButton?: boolean;
+    onClose?: () => void;
+    className?: string;
+}) => {
+    return (
+        <Dialog
+            open={true}
+            onClose={() => {
+                if (onClose) onClose();
+            }}
+            aria-hidden="true"
+            className="fixed inset-0 z-50 flex justify-center items-center w-screen h-screen"
+        >
+            <Dialog.Panel className="bg-white w-full h-full z-50">
+                {closeButton && (
+                    <div className="flex justify-end">
+                        <CloseButton onClose={onClose} />
+                    </div>
+                )}
+                {children}
+            </Dialog.Panel>
+        </Dialog>
+    );
+};
