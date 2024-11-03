54b47c68952531a1475f58b39c9bd7de7090fcfa
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-03 10:18:28 -0300

    dialog en fullscreen popup

--- src/components/base-fullscreen-popup.tsx
+++ src/components/base-fullscreen-popup.tsx
@@ -1,42 +1,37 @@
-import { ReactNode, useEffect, useState } from "react"
-import { createPortal } from "react-dom"
-import { CloseButton } from "./close-button"
+import { ReactNode } from "react";
+import { Dialog } from "@headlessui/react";
+import { CloseButton } from "./close-button";
 
-
-export const BaseFullscreenPopup = ({ children, closeButton = false, onClose, className="px-1" }: {children: ReactNode, closeButton?: boolean, onClose?: () => void, className?: string}) => {
-    const [ready, setReady] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        if (typeof document !== 'undefined' && document.body) {
-            setReady(true);
-            setIsMobile(/Mobi|Android/i.test(navigator.userAgent));
-            document.body.style.overflow = 'hidden'; // Disable scrolling on body
-
-            return () => {
-                document.body.style.overflow = 'auto'; // Re-enable scrolling when component unmounts
-            };
-        }
-    }, []);
-
-    if (!ready) return null;
-
-    return createPortal(
-        <div 
-            className={`inset-0 z-50 flex justify-center items-center ${isMobile ? 'absolute' : 'fixed'}`}
-            style={{ transform: 'translateZ(0)' }} // Force layer creation
+export const BaseFullscreenPopup = ({
+    children,
+    closeButton = false,
+    onClose,
+    className
+}: {
+    children: ReactNode;
+    closeButton?: boolean;
+    onClose?: () => void;
+    className?: string;
+}) => {
+    return (
+        <Dialog
+            open={true}
+            onClose={() => {
+                if (onClose) onClose();
+            }}
+            className="fixed inset-0 z-50 flex justify-center items-center"
         >
-            <div className={className}>
-                <div className="bg-white rounded border-2 border-black text-center z-50 overflow-y-scroll">
+            <div className="fixed inset-0 bg-black opacity-30" aria-hidden="true" />
+            <div className={"relative " + className}>
+                <Dialog.Panel className="bg-white rounded border-2 border-black text-center z-50">
                     {closeButton && (
                         <div className="flex justify-end">
                             <CloseButton onClose={onClose} />
                         </div>
                     )}
                     {children}
-                </div>
+                </Dialog.Panel>
             </div>
-        </div>,
-        document.body
+        </Dialog>
     );
 };
