1f5b5d4833a3ee8ec67ce9686cfaa30438f681f4
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-03 00:28:17 -0300

    fixing write panel main feed

--- src/components/base-fullscreen-popup.tsx
+++ src/components/base-fullscreen-popup.tsx
@@ -23,7 +23,7 @@ export const BaseFullscreenPopup = ({ children, closeButton = false, onClose }: 
     return createPortal(
         <div className="fixed inset-0 z-50 flex justify-center items-center">
             <div className="px-1">
-                <div className="bg-white rounded border-2 border-black text-center max-w-lg z-50">
+                <div className="bg-white rounded border-2 border-black text-center sm:max-w-lg z-50">
                     {closeButton && (
                         <div className="flex justify-end">
                             <CloseButton onClose={onClose} />
