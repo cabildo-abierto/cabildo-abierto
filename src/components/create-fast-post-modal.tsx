887cdb52ba46dfd17f9dc0717f9cf20428b889a6
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-03 00:48:33 -0300

    fixing modals mobile

--- src/components/create-fast-post-modal.tsx
+++ src/components/create-fast-post-modal.tsx
@@ -5,9 +5,7 @@ import { WritePanelMainFeed } from "./write-panel-main-feed";
 
 export const CreateFastPostModal = ({ onClose }: { onClose: () => void }) => {
 
-    return <BaseFullscreenPopup>
-        <div className="w-[98vw] sm:w-128">
-            <WritePanelMainFeed onClose={onClose}/>
-        </div>
+    return <BaseFullscreenPopup className="px-1 sm:w-128 w-[98vh]">
+        <WritePanelMainFeed onClose={onClose}/>
     </BaseFullscreenPopup>
 };
