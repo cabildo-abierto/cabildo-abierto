779f8cd2698a82790957554f2afbecc5b8dc915e
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-03 10:28:40 -0300

    meta viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -172,6 +172,10 @@ export default function RootLayout({
             </script>
 
             <link rel="icon" href={pathLogo} />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+            </meta>
         </head>
         <body className="bg-[var(--background)]">
             {children}
