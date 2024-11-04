02d392f8b077c599c6b343d17fec417dae02c43b
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-04 13:48:21 -0300

    fast post ios

--- src/components/new-fast-post-button.tsx
+++ src/components/new-fast-post-button.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FastPostIcon } from "./icons";
 import InfoPanel from "./info-panel";
 import { CreateFastPostModal } from "./create-fast-post-modal";
+import { isIOS } from 'react-device-detect'
+import { useRouter } from "next/navigation";
 
 
 export const NewFastPostButton = ({onClick, className="", textClassName="", infoPanelIconClassName="text-white", showInfoPanel=true, text}: {onClick: () => void, className?: string, textClassName?: string, infoPanelIconClassName?: string, showInfoPanel?: boolean, text?: string}) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    
+    const router = useRouter()
+  
     return <>
         <button
             onClick={() => {
-                setIsModalOpen(true);
-                onClick()
+                if(!isIOS){
+                    setIsModalOpen(true);
+                    onClick()
+                } else {
+                    router.push("/escribir/rapida")
+                }
             }}
             className={
                 "flex justify-between items-center " + className
