a83f31bf160c2330591078fa8fb7b85e3efb33e8
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-17 11:17:39 -0300

    new presentation

--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -25,24 +25,37 @@ export const ThanksForSubscribing = ({onClose}: {onClose: () => void}) => {
 };
 
 
+export const LogoAndSlogan = () => {
+  return <div className="flex items-center flex-col mb-16">
+        <div className="">
+            <Logo className="lg:w-32 lg:h-32 h-20 w-20"/>
+        </div>
+        <div className="flex justify-center flex-col mt-8">
+            <h1 className="lg:text-5xl text-4xl">Cabildo Abierto</h1>
+            <div className="text-base sm:text-lg text-gray-600 text-left lg:text-2xl my-0 py-0 mt-2">
+                Toda la información en un solo lugar,
+            </div>
+            <div className="text-lg text-gray-600 text-left lg:text-2xl lg:mt-2 my-0 py-0">
+                abierta a discusión.
+            </div>
+        </div>
+    </div>
+}
+
+
+
 export const Presentation = ({loggingIn, setLoggingIn}: {loggingIn: boolean, setLoggingIn: (v: boolean) => void}) => {
 
     const [openThanks, setOpenThanks] = useState(false)
 
-    return <div className="flex flex-col justify-center items-center mt-32 lg:mt-0 px-2">
-      <div className="mb-16 flex lg:h-28 h-16">
-        <div className="">
-          <Logo className="lg:w-28 lg:h-28 h-16 w-16"/>
-        </div>
-        <div className="ml-4 flex h-full justify-center flex-col">
-          <h1 className="lg:text-5xl text-[1.7rem]">Cabildo Abierto</h1>
-          <h2 className="text-gray-600 text-xl lg:text-3xl sm:mt-2 my-0 py-0">Discutí lo público</h2>
-        </div>
-      </div>
-      <Link href={articleUrl("Cabildo_Abierto")} className="text-lg title mb-3 w-72 gray-btn flex justify-center text-center">
+    return <div className="flex flex-col justify-center items-center mt-24 lg:mt-0 px-2">
+      
+      <LogoAndSlogan/>
+
+      <Link href={articleUrl("Cabildo_Abierto")} className="sm:text-lg text-base title mb-3 sm:w-72 w-64 gray-btn flex justify-center text-center">
         <div className="m-1">¿Qué es Cabildo Abierto?</div>
       </Link>
-      <button onClick={() => {setLoggingIn(!loggingIn)}} className="w-72 text-lg title mb-3 green-btn">
+      <button onClick={() => {setLoggingIn(!loggingIn)}} className="sm:w-72 w-64 text-base sm:text-lg title mb-3 green-btn">
         <div className="m-1">{loggingIn ? "Crear cuenta" : "Iniciar sesión"}</div>
       </button>
       {false && <Link href="/inicio" className="link2 mb-16 text-[var(--text-light)] text-sm">
