5b461551fb820886150ddf5cc7b7e78bc731ada0
Tomás Delgado <tmsdlgd@gmail.com>
2024-11-22 15:15:12 -0300

    fix metadata

--- src/app/inicio/[[...route]]/layout.tsx
+++ src/app/inicio/[[...route]]/layout.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: 'Cabildo Abierto',
-    description: 'El lugar donde el país se reúne a discutir. Entrá a conectar con otros y contruir conocimiento colectivo.'
+    description: 'El lugar donde el país se reúne a discutir. Entrá a conectar con otros y construir conocimiento colectivo.'
 }
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HomePage } from "../components/home-page";
 
 export const metadata: Metadata = {
     title: 'Cabildo Abierto',
-    description: 'El lugar donde el país se reúne a discutir. Entrá a conectar con otros y contruir conocimiento colectivo.'
+    description: 'El lugar donde el país se reúne a discutir. Entrá a conectar con otros y construir conocimiento colectivo.'
 }
 
 export default function Page({searchParams}: {searchParams: {code?: string, error_description?: string}}) {
