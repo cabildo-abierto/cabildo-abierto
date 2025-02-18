0e84214a2c938bdc61d23018afbbabf7670f9bd7
Tomás Delgado <tmsdlgd@gmail.com>
2025-02-18 15:26:48 -0300

    inactive icons on sidebar

--- src/components/sidebar-button.tsx
+++ src/components/sidebar-button.tsx
@@ -6,6 +6,7 @@ type SidebarButtonProps = {
     text: string
     onClick: () => void
     icon: ReactNode
+    iconInactive?: ReactNode
     href: string
     disabled?: boolean
     selected?: boolean
@@ -13,7 +14,7 @@ type SidebarButtonProps = {
     setShowText: (showText: boolean) => void
 }
 
-export const SidebarButton: React.FC<SidebarButtonProps> = ({showText, setShowText, text, onClick, icon, href, disabled=false, selected=false}) => {
+export const SidebarButton: React.FC<SidebarButtonProps> = ({showText, setShowText, text, onClick, icon, iconInactive, href, disabled=false, selected=false}) => {
 
     return <>
         <CustomLink href={href} className="mt-1 h-10">
@@ -31,7 +32,7 @@ export const SidebarButton: React.FC<SidebarButtonProps> = ({showText, setShowTe
                 disabled={disabled}
             >
                 <div className={"flex items-center space-x-2"}>
-                {icon} <span className={selected ? "font-bold" : ""}>{showText ? text : ""}</span>
+                {selected || !iconInactive ? icon : iconInactive} <span className={selected ? "font-bold" : ""}>{showText ? text : ""}</span>
                 </div>
             </Button> :
             <Button
@@ -39,7 +40,7 @@ export const SidebarButton: React.FC<SidebarButtonProps> = ({showText, setShowTe
                 variant={"text"}
                 size={"large"}
             >
-                {icon}
+                {selected || !iconInactive ? icon : iconInactive}
             </Button>
             }
         </CustomLink>
