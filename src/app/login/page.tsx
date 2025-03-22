3a2d21ea32aea6789f13474474baa523fc6dc6b8
Tomás Delgado <tmsdlgd@gmail.com>
2025-03-21 22:36:36 -0300

    logo en metadata

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,29 @@
 import { Suspense } from 'react';
 import { Login } from '../../components/auth/login';
+import {Metadata} from "next";
+import {getAvailableInviteCodes} from "../../actions/user/access";
+import {mainDescription} from "../../components/utils/metadata";
+
+
+
+export async function generateMetadata(
+    { searchParams }: {searchParams: Promise<{c: string}>}
+): Promise<Metadata> {
+    const { c } = await searchParams
+
+    if(c){
+        const codes = await getAvailableInviteCodes()
+        if(codes.includes(c)){
+            return {
+                title: "¡Sumate a Cabildo Abierto!",
+                description: mainDescription,
+                icons: {
+                    icon: '/logo.svg',
+                },
+            }
+        }
+    }
+}
 
 
 export default function Page() {
