ce48fee25584c848ce0d9e8d75cd4725824ce033
Tomás Delgado <tmsdlgd@gmail.com>
2025-04-12 11:41:23 -0300

    bingo 2025

--- src/app/bingo/page.tsx
+++ src/app/bingo/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const Page = () => {
     // Create state to track which buttons are toggled (green)
-    const [toggledButtons, setToggledButtons] = useState(Array(100).fill(false));
+    const [toggledButtons, setToggledButtons] = useState(Array(90).fill(false));
     const [showCongrats, setShowCongrats] = useState(false);
 
     // Toggle button color on click
@@ -22,8 +22,8 @@ const Page = () => {
     };
     
     return (
-        <div className="flex justify-between w-screen">
-            <div className="hidden w-64 lg:flex items-end h-screen px-2">
+        <div className="flex justify-between w-screen bg-gray-200">
+            <div className="hidden w-64 sm:flex items-end h-screen px-2">
                 <div className="flex flex-col items-center">
                     <div className="mb-4 flex flex-col items-center text-center">
                         <Image
@@ -44,22 +44,22 @@ const Page = () => {
                     </div>
                 </div>
             </div>
-            <div className="p-4 flex justify-center items-center flex-col">
+            <div className="flex mt-2 items-center justify-center flex-col">
                 <div className="flex justify-center items-center px-2">
-                    <h2 
+                    <div
                         className="text-gray-800 hover:text-blue-600 cursor-pointer text-4xl font-bold text-center"
                         onClick={handleBingoClick}
                     >
                         Bingo!
-                    </h2>
+                    </div>
                 </div>
 
-                <div className="grid grid-cols-10 gap-2 lg:w-[600px] lg:h-[600px] mt-4">
+                <div className="grid grid-cols-10 sm:w-[600px] sm:h-[600px] mt-4">
                     {toggledButtons.map((isToggled, index) => (
                         <button
                             key={index}
                             onClick={() => toggleButton(index)}
-                            className={`lg:w-12 lg:h-12 lg:text-lg rounded-3xl ${isToggled ? 'bg-green-400' : 'bg-white'}`}
+                            className={`border sm:text-2xl sm:w-12 sm:h-12 rounded-full ${isToggled ? 'bg-green-400 hover:bg-green-500' : 'bg-white hover:bg-gray-100'}`}
                         >
                             {index + 1}
                         </button>
@@ -75,7 +75,7 @@ const Page = () => {
                     </div>
                 )}
             </div>
-            <div className="hidden w-64 lg:flex flex-col items-center justify-end h-screen px-4">
+            <div className="hidden w-64 sm:flex flex-col items-center justify-end h-screen px-4">
                 <div className="text-xl text-gray-800 mb-4 flex flex-col items-center">
                     <h3 className="font-bold text-2xl">Cartones</h3>
                     <p>1 x $1000</p>
