191a924b3eb0afa1904e1a50bc6feb7118449335
Tomás Delgado <tmsdlgd@gmail.com>
2025-07-07 21:55:42 -0300

    fix bottom bar

--- src/components/layout/bottom-bar-mobile.tsx
+++ src/components/layout/bottom-bar-mobile.tsx
@@ -4,7 +4,7 @@ import {usePathname, useRouter} from "next/navigation";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "../icons/notifications-icon";
 import TopicsIcon from "@/components/icons/topics-icon";
-import {BottomNavigation, BottomNavigationAction, Paper} from "@mui/material";
+import {BottomNavigation, BottomNavigationAction, Box, Paper} from "@mui/material";
 
 export const BottomBarMobile = () => {
     const pathname = usePathname()
@@ -13,18 +13,43 @@ export const BottomBarMobile = () => {
     const values = ["inicio", "temas", "buscar", "notificaciones"]
     const value: string = values.find(v => pathname.startsWith(`/${v}`)) ?? null
 
-    return <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <BottomNavigation
-            showLabels
-            value={value}
-            onChange={(event, newValue: string) => {
-                router.push(`/${newValue}`)
+    return <Box sx={{display: {xs: 'block', sm: 'block', md: 'none'}}}>
+        <Paper
+            sx={{
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                backgroundColor: "white"
             }}
+            elevation={3}
         >
-            <BottomNavigationAction value="inicio" label="Inicio" icon={<CabildoIcon />} />
-            <BottomNavigationAction value="temas" label="Temas" icon={<TopicsIcon />} />
-            <BottomNavigationAction value="buscar" label="Buscar" icon={<SearchIcon />} />
-            <BottomNavigationAction value="notificaciones" label="Notificaciones" icon={<NotificationsIcon />} />
-        </BottomNavigation>
-    </Paper>
+            <BottomNavigation
+                showLabels
+                value={value}
+                onChange={(event, newValue: string) => {
+                    router.push(`/${newValue}`)
+                }}
+                sx={{
+                    bgcolor: 'var(--background-dark)',
+                    '& .Mui-selected': {
+                        '& .MuiBottomNavigationAction-label': {
+                            fontSize: theme => theme.typography.caption,
+                            transition: 'none',
+                            fontWeight: 'bold',
+                            lineHeight: '20px'
+                        },
+                        '& .MuiSvgIcon-root, & .MuiBottomNavigationAction-label': {
+                            color: theme => 'var(--text)'
+                        }
+                    }
+                }}
+            >
+                <BottomNavigationAction value="inicio" label="Inicio" icon={<CabildoIcon/>}/>
+                <BottomNavigationAction value="temas" label="Temas" icon={<TopicsIcon/>}/>
+                <BottomNavigationAction value="buscar" label="Buscar" icon={<SearchIcon/>}/>
+                <BottomNavigationAction value="notificaciones" label="Notificaciones" icon={<NotificationsIcon/>}/>
+            </BottomNavigation>
+        </Paper>
+    </Box>
 }
