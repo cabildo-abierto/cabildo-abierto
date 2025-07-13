b0089948eb495c6e773c4c9d4a641852de2afbd9
Tomás Delgado <tmsdlgd@gmail.com>
2025-07-13 00:54:08 -0300

    validation icon color

--- src/components/profile/validation-icon.tsx
+++ src/components/profile/validation-icon.tsx
@@ -5,20 +5,20 @@ import { Color } from "../../../modules/ui-utils/src/button";
 import {CheckCircleIcon, UserCircleIcon} from "@phosphor-icons/react";
 
 
-const ValidationIcon = ({handle, validation, fontSize = 22, color="primary"}: {
-    fontSize?: number, handle: string, validation: ValidationState, color?: Color }) => {
+const ValidationIcon = ({handle, validation, fontSize = 22, iconColor="button-text", color="primary"}: {
+    fontSize?: number, handle: string, validation: ValidationState, color?: Color, iconColor?: Color }) => {
     const moreInfoHref = topicUrl("Cabildo Abierto: Verificación de cuentas")
 
     if (validation == "person") {
         return <DescriptionOnHover description={handle && `@${handle} es una persona real.`} moreInfoHref={moreInfoHref}>
             <div style={{backgroundColor: `var(--${color})`}} className={"rounded-full"}>
-                <UserCircleIcon fontSize={fontSize} color={"var(--button-text)"} weight={"fill"}/>
+                <UserCircleIcon fontSize={fontSize} color={`var(--${iconColor}`} weight={"fill"}/>
             </div>
         </DescriptionOnHover>
     } else if (validation == "org") {
         return <DescriptionOnHover description={handle && `@${handle} es una organización verificada.`} moreInfoHref={moreInfoHref}>
             <div style={{backgroundColor: `var(--${color})`}} className={"rounded-full"}>
-                <CheckCircleIcon fontSize={fontSize} color={"var(--button-text)"} weight={"bold"}/>
+                <CheckCircleIcon fontSize={fontSize} color={`var(--${iconColor})`} weight={"bold"}/>
             </div>
         </DescriptionOnHover>
     }
