30b6726692ef4445d7d4720a04e49a278c224ac1
Tomás Delgado <tmsdlgd@gmail.com>
2025-07-16 20:41:40 -0300

    fix reactividad borradores

--- src/components/writing/article/save-draft-article-button.tsx
+++ src/components/writing/article/save-draft-article-button.tsx
@@ -4,8 +4,7 @@ import {post, updateSearchParam} from "@/utils/fetch";
 import {editorStateToMarkdown} from "../../../../modules/ca-lexical-editor/src/markdown-transforms";
 import {ArticleEmbedView} from "@/lex-api/types/ar/cabildoabierto/feed/article";
 import {EmbedContext} from "../../../../modules/ca-lexical-editor/src/nodes/EmbedNode";
-import {useMutation, useQueryClient} from "@tanstack/react-query";
-import {prettyPrintJSON} from "@/utils/strings";
+import {useQueryClient} from "@tanstack/react-query";
 
 type CreateDraftParams = {
     id?: string
@@ -25,34 +24,11 @@ export const SaveDraftArticleButton = ({title, draftId, editorState, disabled, o
 }) => {
     const qc = useQueryClient()
 
-    async function saveDraft(params: CreateDraftParams): Promise<{error: string}> {
-        const saveTime = new Date()
-        const {error, data} = await post<CreateDraftParams, {id: string}>("/draft", params)
-        if(data && data.id && !error){
-            onSavedChanges(saveTime, data.id)
-            updateSearchParam("i", data.id)
-        }
-        return {error}
-    }
-
-    const saveDraftMutation = useMutation<
-        { error: string },
-        unknown,
-        CreateDraftParams
-    >({
-        mutationFn: saveDraft,
-        onSuccess: async () => {
-            await qc.cancelQueries({ queryKey: ["drafts"] })
-            await qc.invalidateQueries({ queryKey: ["drafts"] })
-        }
-    })
-
     async function onSaveDraft() {
         const markdown = editorStateToMarkdown(editorState.toJSON())
 
-        prettyPrintJSON(markdown.embeds)
-        prettyPrintJSON(markdown.embedContexts)
-        const {error} = await saveDraftMutation.mutateAsync({
+        const saveTime = new Date()
+        const {error, data} = await post<CreateDraftParams, {id: string}>("/draft", {
             id: draftId ?? undefined,
             collection: "ar.cabildoabierto.feed.article",
             text: markdown.markdown,
@@ -60,6 +36,14 @@ export const SaveDraftArticleButton = ({title, draftId, editorState, disabled, o
             embedContexts: markdown.embedContexts,
             title: title
         })
+        if(data && data.id && !error){
+            onSavedChanges(saveTime, data.id)
+            updateSearchParam("i", data.id)
+            await qc.cancelQueries({ queryKey: ["drafts"] })
+            await qc.cancelQueries({ queryKey: ["draft", data.id] })
+            await qc.invalidateQueries({ queryKey: ["draft", data.id] })
+            await qc.invalidateQueries({ queryKey: ["drafts"] })
+        }
         return {error}
     }
 
