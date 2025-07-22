42e996b38bbe3b7edec23a87c82eb2413bc43c5d
Tomás Delgado <tmsdlgd@gmail.com>
2025-07-22 14:56:27 -0300

    info menciones y configuracion

--- src/components/topics/topic/topic-feed.tsx
+++ src/components/topics/topic/topic-feed.tsx
@@ -7,7 +7,8 @@ import {topicUrl} from "@/utils/uri";
 import LoadingSpinner from "../../../../modules/ui-utils/src/loading-spinner";
 import { Button } from "../../../../modules/ui-utils/src/button";
 import FeedViewContentFeed from "@/components/feed/feed/feed-view-content-feed";
-import DescriptionOnHover from "../../../../modules/ui-utils/src/description-on-hover";
+import InfoPanel from "../../../../modules/ui-utils/src/info-panel";
+import Link from "next/link"
 
 type TopicFeedOption = "Menciones" | "Discusión"
 type MentionFeedOption = "Publicaciones" | "Temas"
@@ -21,12 +22,8 @@ export const TopicFeed = ({topicId, onClickQuote}: {topicId: string, onClickQuot
     const [mentionsSelected, setMentionsSelected] = useState<MentionFeedOption>("Publicaciones")
 
     function optionsNodes(o: TopicFeedOption, isSelected: boolean){
-        const description = o == "Menciones" ?
-            "Las publicaciones, respuestas, artículos y otros temas que hablaron del tema." :
-            "Discusión sobre el tema y el contenido del artículo."
 
         return <div className="text-[var(--text)] h-10 ">
-            <DescriptionOnHover description={description}>
             <Button
                 variant="text"
                 color="background"
@@ -41,7 +38,6 @@ export const TopicFeed = ({topicId, onClickQuote}: {topicId: string, onClickQuot
                     {o}
                 </div>
             </Button>
-            </DescriptionOnHover>
         </div>
     }
 
@@ -63,16 +59,28 @@ export const TopicFeed = ({topicId, onClickQuote}: {topicId: string, onClickQuot
         </div>
     }
 
-
+    const info = <div>
+        <b>Menciones.</b> Las publicaciones, respuestas, artículos y otros temas que hablaron del tema. <Link
+            target="_blank"
+            className="text-[var(--text-light)] hover:underline"
+            href={topicUrl("Cabildo Abierto: Menciones a temas")}
+        >
+        ¿Cómo detectamos las menciones?
+        </Link>
+        <b>Discusión.</b> Una sección de comentarios hechos directamente sobre el tema. Se puede usar para agregar opiniones.
+    </div>
 
     return <div className={"mb-96"}>
-        <div className={"flex border-b w-full max-w-screen overflow-scroll no-scrollbar"}>
+        <div className={"flex justify-between items-center pr-2 border-b w-full max-w-screen overflow-scroll no-scrollbar"}>
             <SelectionComponent
                 onSelection={setSelected}
                 selected={selected}
                 optionsNodes={optionsNodes}
                 options={["Menciones", "Discusión"]}
                 className={"flex"}
+            />
+            <InfoPanel
+                text={info}
             />
         </div>
         {/*TO DO: BUG: Cuando una respuesta es una mención no debería aparecer línea vertical arriba de la foto de perfil*/}
