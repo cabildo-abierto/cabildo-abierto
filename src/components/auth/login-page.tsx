00943e92c7630a132655579eebea631d7711c175
Tomás Delgado <tmsdlgd@gmail.com>
2025-07-22 17:41:17 -0300

    fix metadata

---
+++ src/components/auth/login-page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import {useSession} from "@/queries/api";
+import {useRouter} from "next/navigation";
+import {LoadingScreen} from "../../../modules/ui-utils/src/loading-screen";
+import {Login} from "@/components/auth/login";
+import {useEffect} from "react";
+
+
+export default function LoginPage() {
+    const session = useSession()
+    const router = useRouter()
+
+    useEffect(() => {
+        if(session.user){
+            router.push("/inicio")
+        }
+    }, [session])
+
+    if(session.isLoading || session.data) return <LoadingScreen/>
+
+    return <div className={"flex items-center justify-center"}>
+        <Login/>
+    </div>
+}
