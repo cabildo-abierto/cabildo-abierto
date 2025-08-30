556488f1c25d5a72edbc90af7cfb5d0d3176a4ce
Luca Zanela <lucazanela@hotmail.com>
2025-08-30 14:02:10 -0300

    actualizo. falta agregar detalles de cantidad de likes y reposts en bsky y en CA

--- src/components/thread/reply-button.tsx
+++ src/components/thread/reply-button.tsx
@@ -1,4 +1,3 @@
-import { Button } from "../../../modules/ui-utils/src/button";
 import {ProfilePic} from "../profile/profile-pic";
 import {useSession} from "@/queries/useSession";
 
@@ -6,17 +5,15 @@ import {useSession} from "@/queries/useSession";
 export const ReplyButton = ({onClick, text = "Responder"}: { onClick: () => void, text?: string }) => {
     const {user} = useSession()
     return <div className="px-2 py-1">
-        <Button
-            color={"background-dark"}
+        <button
             onClick={onClick}
-            sx={{flexDirection: "row", justifyContent: "left"}}
-            className="rounded-full bg-[var(--background-dark3)] w-full hover:bg-[var(--accent)] transition duration-200 flex items-center px-4 py-1 space-x-2"
+            className="rounded-full bg-[var(--background-dark2)] w-full h-9 hover:bg-[var(--background-dark3)] transition duration-200 flex items-center px-2 py-1 space-x-2"
         >
             {user && <>
-                <ProfilePic user={user} className={"pointer-events-none w-8 h-auto rounded-full"} descriptionOnHover={false}/>
-                <span className="text-[var(--text-light)]">{text}</span>
+                <ProfilePic user={user} className={"pointer-events-none w-6 h-auto rounded-full"} descriptionOnHover={false}/>
+                <span className="text-sm text-[var(--text-light)]">{text}</span>
             </>}
             {!user && <div className={"text-[var(--text-light)]"}>Iniciá sesión para responder y ver las respuestas de otros</div>}
-        </Button>
+        </button>
     </div>
 }
