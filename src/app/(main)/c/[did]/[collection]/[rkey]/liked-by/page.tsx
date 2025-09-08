bd26725bf7e770518b219341dc0e326dcb696e72
Luca Zanela <lucazanela@hotmail.com>
2025-09-08 20:54:14 -0300

    falta cantidad de citas de CA

--- src/app/(main)/c/[did]/[collection]/[rkey]/liked-by/page.tsx
+++ src/app/(main)/c/[did]/[collection]/[rkey]/liked-by/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import PageHeader from "../../../../../../../../modules/ui-utils/src/page-header";
-import InfoPanel from "../../../../../../../../modules/ui-utils/src/info-panel";
-import {getUri, shortCollectionToCollection, topicUrl} from "@/utils/uri";
+import {getUri, shortCollectionToCollection} from "@/utils/uri";
 import {DetailsContent} from "@/components/thread/details-content";
 import React from "react";
 
@@ -15,7 +14,7 @@ export default function Page({params}: {
     const uri = getUri(decodeURIComponent(did), shortCollectionToCollection(collection), rkey)
 
     return <div className={"pb-16"}>
-        <PageHeader title={"Le gusta a "} rightSide={<InfoPanel moreInfoHref={topicUrl("Cabildo Abierto: Cuentas para seguir", undefined, "normal")}/>}/>
+        <PageHeader title={"Le gusta a "}/>
         <DetailsContent detail={"likes"}
                         uri = {uri}/>
     </div>
