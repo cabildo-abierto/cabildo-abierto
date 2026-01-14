d5a5ebbb4c0bb5467734a266422e7eb8d21636c7
Tomás Delgado <tmsdlgd@gmail.com>
2026-01-13 21:24:57 -0300

    logo bsky mas chico

--- apps/web/src/components/utils/icons/bluesky-logo.tsx
+++ apps/web/src/components/utils/icons/bluesky-logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const BlueskyLogo = ({className}: {className?: string}) => {
     return <Image
-        src={"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/900px-Bluesky_Logo.svg.png"}
+        src={"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/330px-Bluesky_Logo.svg.png"}
         alt={"Bluesky"}
         width={300}
         height={300}
